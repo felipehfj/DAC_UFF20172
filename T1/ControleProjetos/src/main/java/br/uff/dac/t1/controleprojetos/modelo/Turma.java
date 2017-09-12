120053de39f7333184561b276cb002d317e31d64
Felipe Ferreira <felipehfj@gmail.com>
2017-09-12 16:30:42 -0300

    Adição MYSQL no pom.xml

--- T1/ControleProjetos/src/main/java/br/uff/dac/t1/controleprojetos/modelo/Turma.java
+++ T1/ControleProjetos/src/main/java/br/uff/dac/t1/controleprojetos/modelo/Turma.java
@@ -5,6 +5,8 @@
  */
 package br.uff.dac.t1.controleprojetos.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -12,7 +14,10 @@ import java.util.Objects;
  *
  * @author felipe
  */
+
+@Entity
 public class Turma implements Serializable{
+    @Id
     private int id;
     private String codigo;
     private String periodo;
