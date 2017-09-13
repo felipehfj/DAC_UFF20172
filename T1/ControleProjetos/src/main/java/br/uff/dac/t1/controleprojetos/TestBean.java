43f38f44b6ee9e6d6758856026d9165c8330d6b8
Felipe Gomes <ogomes.felipe@gmail.com>
2017-09-13 00:28:37 -0300

    Jsf

--- T1/ControleProjetos/src/main/java/br/uff/dac/t1/controleprojetos/TestBean.java
+++ T1/ControleProjetos/src/main/java/br/uff/dac/t1/controleprojetos/TestBean.java
@@ -1,14 +1,24 @@
 package br.uff.dac.t1.controleprojetos;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean(name = "test", eager = true)
-@SessionScoped
+@RequestScoped
 public class TestBean {
+
+    private String nome;
 
     public String getMessage() {
         return "Test works!";
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
